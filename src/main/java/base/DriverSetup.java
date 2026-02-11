83363171ab2cb1b32978436c78d154a815e3b8db
Jagadishwar Reddy <jagadish1624@gmail.com>
2026-02-11 15:30:10 +0000

    ParaBank9

--- src/main/java/base/DriverSetup.java
+++ src/main/java/base/DriverSetup.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverSetup {
@@ -22,6 +24,15 @@ public class DriverSetup {
     	options.addArguments("--disable-infobars");
     	options.addArguments("--disable-autofill");
 
+    	EdgeOptions edgeoptions = new EdgeOptions();
+    	 edgeoptions.addArguments("--headless=new");
+         edgeoptions.addArguments("--disable-gpu");
+         edgeoptions.addArguments("--window-size=1920,1080");
+         edgeoptions.addArguments("--disable-notifications");
+     	edgeoptions.addArguments("--disable-save-password-bubble");
+     	edgeoptions.addArguments("--disable-infobars");
+     	edgeoptions.addArguments("--disable-autofill");
+
         String browser = ConfigLoader.get("browser");
 
         if(browser.equalsIgnoreCase("chrome")) {
@@ -30,7 +41,7 @@ public class DriverSetup {
         }
         else {
             WebDriverManager.edgedriver().setup();
-            driver.set(new EdgeDriver());
+            driver.set(new EdgeDriver(edgeoptions));
         }
 
         getDriver().manage().window().maximize();
